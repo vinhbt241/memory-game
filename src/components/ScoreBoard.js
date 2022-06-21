35abc0c74b4d9c12518d23e397beae81cbce1da2
Vinh <vinhbuith32412000@gmail.com>
2022-06-21 19:07:21 +0700

    Create static score board

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const ScoreBoard = (props) => {
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <p>Get points by clicking on an image but don't click on any more than once!</p>
+
+      {props.score}
+    </div>
+  )
+}
+
+export { ScoreBoard };
